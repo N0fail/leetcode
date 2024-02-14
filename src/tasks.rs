135391fce3a6ba423e6df4c69e7098cc85d0acd3
Konstantin Kudryavtsev <suuupport2@gmail.com>
2024-02-14 20:56:39 +0300

    add 2498

--- src/tasks.rs
+++ src/tasks.rs
@@ -54,8 +54,9 @@ pub mod task0198;
 pub mod task0661;
 pub mod task2149;
 pub mod task2535;
+pub mod task2498;
 
-pub use task2535 as current;
+pub use task2498 as current;
 
 pub trait Solver{
     fn read_inputs() -> Self;
---
+++ src/tasks/task2498.rs
@@ -0,0 +1,45 @@
+use super::Solver;
+
+#[derive(Debug)]
+pub struct Solution{
+    stones: Vec<i32>,
+    solution: i32,
+}
+
+// struct Accum {
+//     el: i32,
+//     prev_el: i32,
+//     res: i32,
+// }
+
+// pub fn max_jump(stones: Vec<i32>) -> i32 {
+//     11 ms
+//     stones.into_iter().fold(Accum{el:0,prev_el:0,res:0}, |acc, stone|{
+//         Accum{
+//             el: stone,
+//             prev_el: acc.el,
+//             res: acc.res.max(stone-acc.prev_el)
+//         }
+//     }).res
+// }
+
+pub fn max_jump(stones: Vec<i32>) -> i32 {
+    // 3ms
+    stones.windows(3).fold(stones[1] - stones[0], |res, elems|{
+        res.max(elems[2] - elems[0])
+    })
+}
+
+impl Solver for Solution {
+    fn read_inputs() -> Self {
+        return Solution {
+            stones: vec![1,2],
+            solution: -1,
+        };
+    }
+
+    fn solve(mut self) {
+        self.solution = max_jump(self.stones);
+        dbg!(self.solution);
+    }
+}
