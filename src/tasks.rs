953a4118e4183085a78a5481a96c31bcae1d0f10
Konstantin Kudryavtsev <suuupport2@gmail.com>
2024-02-17 10:35:16 +0300

    add 1770

--- src/tasks.rs
+++ src/tasks.rs
@@ -2,73 +2,73 @@ use std::cell::RefCell;
 use std::fmt::{Debug, Display, Formatter};
 use std::rc::Rc;
 
-pub mod task0542;
+pub mod task0049;
+pub mod task0076;
+pub mod task0119;
+pub mod task0150;
+pub mod task0169;
+pub mod task0198;
+pub mod task0222;
+pub mod task0232;
+pub mod task0242;
+pub mod task0279;
+pub mod task0301;
+pub mod task0336;
+pub mod task0368;
+pub mod task0378;
+pub mod task0387;
+pub mod task0451;
 pub mod task0518;
 pub mod task0521;
 pub mod task0538;
-pub mod task0336;
-pub mod task0378;
-pub mod task0232;
-pub mod task2731;
-pub mod task0150;
-pub mod task0931;
+pub mod task0542;
+pub mod task0647;
+pub mod task0661;
 pub mod task0739;
-pub mod task0222;
-pub mod task2966;
-pub mod task1291;
-pub mod task1657;
-pub mod task1043;
-pub mod task1095;
-pub mod task0076;
-pub mod task0915;
-pub mod task0881;
-pub mod task0845;
+pub mod task0806;
+pub mod task0815;
 pub mod task0820;
 pub mod task0833;
-pub mod task0806;
+pub mod task0845;
 pub mod task0879;
-pub mod task0387;
-pub mod task1529;
-pub mod task1535;
-pub mod task1477;
-pub mod task0049;
-pub mod task1980;
-pub mod task0451;
+pub mod task0881;
+pub mod task0907;
+pub mod task0915;
+pub mod task0931;
+pub mod task1043;
+pub mod task1095;
+pub mod task1291;
+pub mod task1347;
 pub mod task1365;
 pub mod task1456;
-pub mod task0279;
-pub mod task0301;
-pub mod task0368;
-pub mod task1347;
-pub mod task0907;
-pub mod task1793;
-pub mod task0647;
-pub mod task1846;
-pub mod task0242;
-pub mod task2251;
-pub mod task0169;
-pub mod task2785;
-pub mod task2108;
-pub mod task1503;
-pub mod task0198;
-pub mod task0661;
-pub mod task2149;
-pub mod task2535;
-pub mod task2498;
-pub mod task2442;
-pub mod task0815;
-pub mod task2971;
+pub mod task1477;
 pub mod task1481;
-pub mod task0119;
+pub mod task1503;
+pub mod task1529;
+pub mod task1535;
+pub mod task1657;
+pub mod task1770;
+pub mod task1793;
+pub mod task1846;
 pub mod task1897;
+pub mod task1980;
+pub mod task2108;
+pub mod task2149;
+pub mod task2251;
+pub mod task2442;
+pub mod task2498;
+pub mod task2535;
+pub mod task2731;
+pub mod task2785;
+pub mod task2966;
+pub mod task2971;
 
-pub use task1897 as current;
+pub use task1770 as current;
 
-pub trait Solver{
+pub trait Solver {
     fn read_inputs() -> Self;
     fn solve(self);
 }
-
 
 pub const NULL: i32 = -1_000_000;
 #[derive(Debug, PartialEq, Eq)]
@@ -84,28 +84,28 @@ impl TreeNode {
         TreeNode {
             val,
             left: None,
-            right: None
+            right: None,
         }
     }
 
-    pub fn to_vec(&self, my_idx: usize, output: & mut Vec<i32>) {
+    pub fn to_vec(&self, my_idx: usize, output: &mut Vec<i32>) {
         if output.len() <= my_idx {
-            output.resize(my_idx+1, NULL)
+            output.resize(my_idx + 1, NULL)
         }
         output[my_idx] = self.val;
         if let Some(left) = &self.left {
-            left.borrow().to_vec(my_idx*2+1, output);
+            left.borrow().to_vec(my_idx * 2 + 1, output);
         }
         if let Some(right) = &self.right {
-            right.borrow().to_vec(my_idx*2+2, output);
+            right.borrow().to_vec(my_idx * 2 + 2, output);
         }
     }
 }
 
 impl Display for TreeNode {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        let mut result: Vec<i32> =  vec![];
-        self.to_vec(0, & mut result);
+        let mut result: Vec<i32> = vec![];
+        self.to_vec(0, &mut result);
         let mut print_res: Vec<String> = vec![];
         print_res.reserve(result.len());
         for value in &result {
@@ -119,7 +119,6 @@ impl Display for TreeNode {
     }
 }
 
-
 pub fn build_tree(input: &Vec<i32>, cur_idx: usize) -> Option<Rc<RefCell<TreeNode>>> {
     if cur_idx >= input.len() {
         return None;
@@ -129,8 +128,8 @@ pub fn build_tree(input: &Vec<i32>, cur_idx: usize) -> Option<Rc<RefCell<TreeNod
     }
 
     let new_node = create_node(input[cur_idx]);
-    new_node.borrow_mut().left = build_tree(input, cur_idx*2 + 1);
-    new_node.borrow_mut().right = build_tree(input, cur_idx*2 + 2);
+    new_node.borrow_mut().left = build_tree(input, cur_idx * 2 + 1);
+    new_node.borrow_mut().right = build_tree(input, cur_idx * 2 + 2);
     return Some(new_node);
 }
 
---
+++ src/tasks/task1770.rs
@@ -0,0 +1,134 @@
+use super::Solver;
+
+#[derive(Debug)]
+pub struct Solution {
+    nums: Vec<i32>,
+    multipliers: Vec<i32>,
+    solution: i32,
+}
+
+// fn rec(nums: &[i32], multipliers: &[i32], left_picked: usize, right_picked: usize, cache: &mut[Option<i32>;1000_000]) -> i32 {
+//     // 32 ms
+//     if multipliers.len() == 0 {
+//         return 0
+//     }
+//     if let Some(x) = cache[left_picked*1000+right_picked] {
+//         return x
+//     }
+//     let left = nums[0] * multipliers[0] + rec(&nums[1..], &multipliers[1..], left_picked+1, right_picked, cache);
+//     let right = nums[nums.len()-1] * multipliers[0] + rec(&nums[..nums.len()-1], &multipliers[1..], left_picked, right_picked+1, cache);
+//     let res = left.max(right);
+//     cache[left_picked*1000+right_picked] = Some(res);
+//     res
+// }
+//
+// pub fn maximum_score(nums: Vec<i32>, multipliers: Vec<i32>) -> i32 {
+//     rec(&nums, &multipliers, 0, 0, &mut [None;1000_000])
+// }
+
+// fn rec(nums: &[i32], multipliers: &[i32], left_picked: usize, right_picked: usize, cache: &mut[i32;1000_000]) -> i32 {
+//     // 24ms
+//     if multipliers.len() == 0 {
+//         return 0
+//     }
+//     if cache[left_picked*1000+right_picked] == i32::MAX {
+//         let left = nums[0] * multipliers[0] + rec(&nums[1..], &multipliers[1..], left_picked+1, right_picked, cache);
+//         let right = nums[nums.len()-1] * multipliers[0] + rec(&nums[..nums.len()-1], &multipliers[1..], left_picked, right_picked+1, cache);
+//         let res = left.max(right);
+//         cache[left_picked*1000+right_picked] = res;
+//     }
+//     cache[left_picked*1000+right_picked]
+// }
+//
+// pub fn maximum_score(nums: Vec<i32>, multipliers: Vec<i32>) -> i32 {
+//     rec(&nums, &multipliers, 0, 0, &mut [i32::MAX;1000_000])
+// }
+
+// fn rec(nums: &[i32], multipliers: &[i32], left_picked: usize, right_picked: usize, cache: &mut[i32;1000_000]) -> i32 {
+//     // 18ms
+//     if multipliers.len() == left_picked+right_picked {
+//         return 0
+//     }
+//     if cache[left_picked*1000+right_picked] == i32::MAX {
+//         let left = nums[left_picked] * multipliers[left_picked+right_picked] + rec(nums, multipliers, left_picked+1, right_picked, cache);
+//         let right = nums[nums.len()-right_picked-1] * multipliers[left_picked+right_picked] + rec(nums, multipliers, left_picked, right_picked+1, cache);
+//         let res = left.max(right);
+//         cache[left_picked*1000+right_picked] = res;
+//     }
+//     cache[left_picked*1000+right_picked]
+// }
+//
+// impl Solution {
+//     pub fn maximum_score(nums: Vec<i32>, multipliers: Vec<i32>) -> i32 {
+//         rec(&nums, &multipliers, 0, 0, &mut [i32::MAX;1000_000])
+//     }
+// }
+
+// fn rec(nums: &[i32], multipliers: &[i32], left_picked: usize, right_picked: usize, cache: &mut [[i32; 1000]; 1000]) -> i32 {
+//     // 25 ms
+//     if multipliers.len() == left_picked+right_picked {
+//         return 0
+//     }
+//     if cache[left_picked][right_picked] == i32::MAX {
+//         let left = nums[left_picked] * multipliers[left_picked+right_picked] + rec(nums, multipliers, left_picked+1, right_picked, cache);
+//         let right = nums[nums.len()-right_picked-1] * multipliers[left_picked+right_picked] + rec(nums, multipliers, left_picked, right_picked+1, cache);
+//         let res = left.max(right);
+//         cache[left_picked][right_picked] = res;
+//     }
+//     cache[left_picked][right_picked]
+// }
+//
+// pub fn maximum_score(nums: Vec<i32>, multipliers: Vec<i32>) -> i32 {
+//     rec(&nums, &multipliers, 0, 0, &mut [[i32::MAX; 1000]; 1000])
+// }
+
+pub fn maximum_score(nums: Vec<i32>, multipliers: Vec<i32>) -> i32 {
+    // unrolled recursion
+    let m = multipliers.len();
+    let n = nums.len();
+    let mut cache = vec![vec![0; m + 1]; m + 1]; // 11 ms
+                                                 // let mut cache = [[0; 1001]; 1001]; // 15 ms
+    for total in (0..m).rev() {
+        for left_picked in (0..=total).rev() {
+            let right_picked = total - left_picked;
+            let left =
+                nums[left_picked] * multipliers[total] + cache[left_picked + 1][right_picked];
+            let right = nums[n - right_picked - 1] * multipliers[total]
+                + cache[left_picked][right_picked + 1];
+            cache[left_picked][right_picked] = left.max(right);
+        }
+    }
+    cache[0][0]
+}
+
+// pub fn maximum_score(nums: Vec<i32>, multipliers: Vec<i32>) -> i32 {
+//     // unrolled recursion
+//     let m = multipliers.len();
+//     let n = nums.len();
+//     // let mut cache = vec![0; (m+1)*(m+1)]; // 14 ms
+//     let mut cache = [0; 1001 * 1001]; // 20ms
+//     for total in (0..m).rev() {
+//         for left_picked in (0..=total).rev() {
+//             let right_picked = total - left_picked;
+//             let left = nums[left_picked] * multipliers[total] + cache[(left_picked+1)*m+right_picked];
+//             let right = nums[n-right_picked-1] * multipliers[total] + cache[left_picked*m+right_picked+1];
+//             cache[left_picked*m+right_picked] = left.max(right);
+//         }
+//     }
+//     cache[0]
+// }
+
+impl Solver for Solution {
+    fn read_inputs() -> Self {
+        return Solution {
+            nums: vec![1, 2, 3],
+            multipliers: vec![3, 2, 1],
+            solution: -1,
+        };
+    }
+
+    fn solve(mut self) {
+        self.solution = maximum_score(self.nums, self.multipliers);
+        dbg!(self.solution);
+    }
+}
